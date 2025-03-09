fc18eaf7ae5ef06c660319ac495d5ef18e2ba282
Kaye Gamana <kayecgamana@gmail.com>
2025-03-10 00:36:35 +0800

    added new folder ADET

---
+++ ADET/assets/js/script.js
@@ -0,0 +1,67 @@
+// NAVIGATION
+var navLinks = [
+    { text: "Home", href: "../index.html" },
+    { text: "About", href: "../index.html#aboutMe" },
+    { text: "Projects", href: "../index.html#projects" },
+    { text: "Contact", href: "../index.html#contact" }
+];
+
+var navbarNavList = document.getElementById("navbarNavList");
+
+for (var i = 0; i < navLinks.length; i++) {
+    navbarNavList.innerHTML += `
+        <div class="nav-item">
+            <a class="nav-link" href="` + navLinks[i].href + `" style="color: white; text-decoration: none; transition: text-shadow 0.3s ease;"
+            onmouseover="this.style.color='#f1c40f'; this.style.textShadow='2px 2px 5px rgba(0, 0, 0, 0.5)';"
+            onmouseout="this.style.color='white'; this.style.textShadow='none';">` + navLinks[i].text + `</a>
+        </div>`;
+}
+
+// PROJECTS showcase
+var projects = [
+    {
+        link: "../project%201/index.html",
+        preview: "assets/images/projectPreviewComingSoon.png",
+        hover: "assets/images/projectPreviewComingSoon.png",
+        alt: "Project1"
+    },
+    {
+        link: "../project%202/index.html",
+        preview: "assets/images/projectPreviewComingSoon.png",
+        hover: "assets/images/projectPreviewComingSoon.png",
+        alt: "Project2"
+    },
+    {
+        link: "../project%203/index.html",
+        preview: "assets/images/projectPreviewComingSoon.png",
+        hover: "assets/images/projectPreviewComingSoon.png",
+        alt: "Project3"
+    },
+    {
+        link: "../project%204/index.html",
+        preview: "assets/images/projectPreviewComingSoon.png",
+        hover: "assets/images/projectPreviewComingSoon.png",
+        alt: "Project4"
+    },
+    {
+        link: "../project%205/index.html",
+        preview: "assets/images/projectPreviewComingSoon.png",
+        hover: "assets/images/projectPreviewComingSoon.png",
+        alt: "Project5"
+    },
+    {
+        link: "../project%206/index.html",
+        preview: "assets/images/projectPreviewComingSoon.png",
+        hover: "assets/images/projectPreviewComingSoon.png",
+        alt: "Project6"
+    }
+];
+
+var projectsContainer = document.getElementById('projectsContainer');
+
+for (var i = 0; i < projects.length; i++) {
+    projectsContainer.innerHTML += `
+        <div class='col-md-4 mb-4'>
+            <img src=` + projects[i].preview + ` alt=` + projects[i].alt + ` class='img-fluid project-img'>
+        </div>`;
+}
