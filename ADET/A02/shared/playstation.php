bcbf101be8ef43af403a19581effb2f8901237f7
Kaye Gamana <kayecgamana@gmail.com>
2025-04-14 00:43:59 +0800

    added file - new page

---
+++ ADET/A02/shared/playstation.php
@@ -0,0 +1,105 @@
+<div class="playstation container">
+
+    <!-- TITLE -->
+    <h2 class="mt-4 mb-3" style="font-weight: 900; color:rgb(7, 43, 101);">Playstation News</h2>
+
+    <!-- IMAGE BANNER -->
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="margin-bottom: 1.8rem;">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/playstation/banner1.png" class="d-block"
+                    style="width: 100%; height: auto; object-fit: cover; border-radius: 10px;">
+            </div>
+            <div class="carousel-item">
+                <img src="images/playstation/banner2.png" class="d-block"
+                    style="width: 100%; height: auto; object-fit: cover; border-radius: 10px;">
+            </div>
+            <div class="carousel-item">
+                <img src="images/playstation/banner3.png" class="d-block"
+                    style="width: 100%; height: auto; object-fit: cover; border-radius: 10px;">
+            </div>
+        </div>
+    </div>
+
+    <!-- NEWS ARTICLE CARDS -->
+    <!-- CARD 1 -->
+    <a href="?page=gamingNewsArticle1" class="text-decoration-none text-dark">
+        <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="images/playstation/azureLatchCodes.png" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h6 class="card-title mt-3" style="font-weight:600;">Codes for Azure Latch (April 2025)</h6>
+                        <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">18h ago - Looking for cash to unlock
+                            styles in Azure Latch? Use these codes!</p>
+                        <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">LAUREN HARPER</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <hr style="height: 1px; max-width: 850px; margin: 1rem 0;">
+
+    <!-- CARD 2 -->
+    <a href="?page=gamingNewsArticle1" class="text-decoration-none text-dark">
+        <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="images/playstation/spongebobTowerDefense.png" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h6 class="card-title mt-3" style="font-weight:600;">Codes for SpongeBob Tower Defense (April
+                            2025)</h6>
+                        <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">19h ago - Redeem these codes to boost
+                            your units and help protect Bikini Bottom!</p>
+                        <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">LAUREN HARPER</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <hr style="height: 1px; max-width: 850px; margin: 1rem 0;">
+
+    <!-- CARD 3 -->
+    <a href="?page=gamingNewsArticle1" class="text-decoration-none text-dark">
+        <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="images/playstation/basketballZeroCodes.png" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h6 class="card-title mt-3" style="font-weight:600;">Basketball: Zero Codes (April 2025)</h6>
+                        <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">19h ago - We've got all the codes you
+                            need to know for cash, Lucky Spins, and more!</p>
+                        <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">LAUREN HARPER</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <hr style="height: 1px; max-width: 850px; margin: 1rem 0;">
+
+    <!-- CARD 4 -->
+    <a href="?page=gamingNewsArticle1" class="text-decoration-none text-dark">
+        <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="images/playstation/attackOnTitanRevolution.jpg" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h6 class="card-title mt-3" style="font-weight:600;">Attack on Titan Revolution Codes (April
+                            2025)</h6>
+                        <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">21h ago - Redeem these Attack on Titan
+                            Revolution codes for extra Spins, Luck Potions, and more.</p>
+                        <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">LAUREN HARPER</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+</div>
