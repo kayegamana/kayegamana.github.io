a959a209040a13829ba01c503b576fa7cdef5301
Kaye Gamana <kayecgamana@gmail.com>
2025-04-14 00:24:13 +0800

    added file - new page

---
+++ ADET/A02/shared/nintendo.php
@@ -0,0 +1,110 @@
+<div class="nintendo container">
+
+    <!-- TITLE -->
+    <h2 class="mt-4 mb-3" style="font-weight: 900; color:rgb(7, 43, 101);">Nintendo News</h2>
+
+    <!-- IMAGE BANNER -->
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="margin-bottom: 1.8rem;">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/nintendo/banner1.png" class="d-block"
+                    style="width: 100%; height: auto; object-fit: cover; border-radius: 10px;">
+            </div>
+            <div class="carousel-item">
+                <img src="images/nintendo/banner2.png" class="d-block"
+                    style="width: 100%; height: auto; object-fit: cover; border-radius: 10px;">
+            </div>
+            <div class="carousel-item">
+                <img src="images/nintendo/banner3.png" class="d-block"
+                    style="width: 100%; height: auto; object-fit: cover; border-radius: 10px;">
+            </div>
+        </div>
+    </div>
+
+    <!-- NEWS ARTICLE CARDS -->
+    <!-- CARD 1 -->
+    <a href="?page=gamingNewsArticle3" class="text-decoration-none text-dark">
+        <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="images/nintendo/theLegendOfZelda.jpg" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h6 class="card-title mt-3" style="font-weight:600;">Zelda: Breath of the Wild Switch 2 Edition
+                            Won’t Include DLC</h6>
+                        <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">1d ago - Boost your SkyBux balance and
+                            upgrade your airline with these Cabin Crew Simulator codes.</p>
+                        <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">LAUREN HARPER</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+
+    <hr style="height: 1px; max-width: 850px; margin: 1rem 0;">
+
+    <a href="?page=gamingNewsArticle3" class="text-decoration-none text-dark">
+        <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="images/nintendo/superMarioPartyJamboree.jpg" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h6 class="card-title mt-3" style="font-weight:600;">The Best Deals Today: Super Mario Party
+                            Jamboree, Middle-Earth 4K Film Collection, and More</h6>
+                        <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">2d ago - You'll have to pay up.</p>
+                        <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">REBEKAH
+                            VALENTINE</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+
+    <hr style="height: 1px; max-width: 850px; margin: 1rem 0;">
+
+    <a href="?page=gamingNewsArticle3" class="text-decoration-none text-dark">
+        <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="images/nintendo/fortniteOgSeason1.jpg" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h6 class="card-title mt-3" style="font-weight:600;">Battle Royale Interest is Declining But
+                            Fortnite Continues To Dominate, New Report Says</h6>
+                        <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">3d ago - King of the hill.</p>
+                        <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">ERIC VAN ALLEN</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+
+    <hr style="height: 1px; max-width: 850px; margin: 1rem 0;">
+
+    <a href="?page=gamingNewsArticle3" class="text-decoration-none text-dark">
+        <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="images/nintendo/astroBot.jpg" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h6 class="card-title mt-3" style="font-weight:600;">BAFTA Games Awards 2025: The Full List of
+                            Winners</h6>
+                        <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">3d ago - Astro Bot and Still Wakes the
+                            Deep the night's biggest winners.</p>
+                        <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">VIKKI BLAKE</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+</div>
