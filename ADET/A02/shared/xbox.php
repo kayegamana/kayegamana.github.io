e07c3b1e58fdfeeb8d2ca3911bc506f57a3741f7
Kaye Gamana <kayecgamana@gmail.com>
2025-04-14 00:55:55 +0800

    added file - new page

---
+++ ADET/A02/shared/xbox.php
@@ -0,0 +1,115 @@
+<div class="xbox container">
+
+    <!-- TITLE -->
+    <h2 class="mt-4 mb-3" style="font-weight: 900; color:rgb(7, 43, 101);">Xbox News</h1>
+
+        <!-- IMAGE BANNER -->
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel"
+            style="margin-bottom: 1.8rem;">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/xbox/banner1.png" class="d-block"
+                        style="width: 100%; height: auto; object-fit: cover; border-radius: 10px;">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/xbox/banner2.png" class="d-block"
+                        style="width: 100%; height: auto; object-fit: cover; border-radius: 10px;">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/xbox/banner3.png" class="d-block"
+                        style="width: 100%; height: auto; object-fit: cover; border-radius: 10px;">
+                </div>
+            </div>
+        </div>
+
+        <!-- NEWS ARTICLE CARDS -->
+        <!-- CARD 1 -->
+        <a href="?page=gamingNewsArticle2" class="text-decoration-none text-dark">
+            <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="images/xbox/untitledBoxingGameCodes.jpg" class="img-fluid rounded-start">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h6 class="card-title mt-3" style="font-weight:600;">Untitled Boxing Game Codes July 2024
+                            </h6>
+                            <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">1d ago - Every Untitled Boxing
+                                Game code available to claim this July.</p>
+                            <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">CALLUM
+                                WILLIAMS</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <hr style="height: 1px; max-width: 850px; margin: 1rem 0;">
+
+        <!-- CARD 2 -->
+        <a href="?page=gamingNewsArticle2" class="text-decoration-none text-dark">
+            <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="images/xbox/cabinCrewSimulatorCodes.jpg" class="img-fluid rounded-start">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h6 class="card-title mt-3" style="font-weight:600;">Cabin Crew Simulator Codes (April 2025)
+                            </h6>
+                            <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">1d ago - Boost your SkyBux balance
+                                and
+                                upgrade your airline with these Cabin Crew Simulator codes.</p>
+                            <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">LAUREN
+                                HARPER</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <hr style="height: 1px; max-width: 850px; margin: 1rem 0;">
+
+        <!-- CARD 3 -->
+        <a href="?page=gamingNewsArticle2" class="text-decoration-none text-dark">
+            <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="images/xbox/bloxFruitsCodes.jpg" class="img-fluid rounded-start">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h6 class="card-title mt-3" style="font-weight:600;">Blox Fruits Codes (April 2025)</h6>
+                            <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">1d ago - Redeem these Blox Fruits
+                                codes for double EXP, stat resets, and more!</p>
+                            <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">MEG KOEPP</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <hr style="height: 1px; max-width: 850px; margin: 1rem 0;">
+
+        <!-- CARD 4 -->
+        <a href="?page=gamingNewsArticle2" class="text-decoration-none text-dark">
+            <div class="card news-card mt-3 mb-3" style="max-width: 850px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="images/xbox/dandysWorldCodes.jpg" class="img-fluid rounded-start">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h6 class="card-title mt-3" style="font-weight:600;">Dandy's World Codes (April 2025)</h6>
+                            <p class="card-text mt-4 mb-1" style="font-size: 0.8rem;">1d ago - Use these redeemable
+                                Dandy's
+                                World codes for a ton of free Ichor!</p>
+                            <p class="card-text mt-1 text-body-secondary" style="font-size: 0.7rem;">CALLUM
+                                WILLIAMS</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+</div>
