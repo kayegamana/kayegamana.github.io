c339c266edfa4b3e0cb93bacc97ee77ba8abd599
Kaye Gamana <kayecgamana@gmail.com>
2025-04-14 09:42:59 +0800

    refined proj preview

--- ADET/assets/js/script.js
+++ ADET/assets/js/script.js
@@ -17,40 +17,29 @@ for (var i = 0; i < navLinks.length; i++) {
         </div>`;
 }
 
-// PROJECTS showcase
+// Coming Soon - PROJECTS showcase
 var projects = [
     {
-        link: "A02/index.php",
-        preview: "assets/images/projectPreview1.png",
-        hover: "assets/images/projectPreviewHover1.png",
-        alt: "Project1"
-    },
-    {
-        link: "project%202/index.html",
         preview: "assets/images/projectPreviewComingSoon.png",
         hover: "assets/images/projectPreviewComingSoon.png",
         alt: "Project2"
     },
     {
-        link: "project%203/index.html",
         preview: "assets/images/projectPreviewComingSoon.png",
         hover: "assets/images/projectPreviewComingSoon.png",
         alt: "Project3"
     },
     {
-        link: "project%204/index.html",
         preview: "assets/images/projectPreviewComingSoon.png",
         hover: "assets/images/projectPreviewComingSoon.png",
         alt: "Project4"
     },
     {
-        link: "project%205/index.html",
         preview: "assets/images/projectPreviewComingSoon.png",
         hover: "assets/images/projectPreviewComingSoon.png",
         alt: "Project5"
     },
     {
-        link: "project%206/index.html",
         preview: "assets/images/projectPreviewComingSoon.png",
         hover: "assets/images/projectPreviewComingSoon.png",
         alt: "Project6"
@@ -62,11 +51,8 @@ var projectsContainer = document.getElementById('projectsContainer');
 for (var i = 0; i < projects.length; i++) {
     projectsContainer.innerHTML += `
         <div class='col-md-4 mb-4'>
-            <a href=` + projects[i].link + ` target='_blank'>
-                <div class='project-img-container'>
-                    <img src=` + projects[i].preview + ` alt=` + projects[i].alt + ` class='img-fluid project-img'>
-                    <img src=` + projects[i].hover + ` alt=` + projects[i].alt + `Hover' class='img-fluid project-hover-img'>
-                </div>
-            </a>
+            <div class='cs-project-img-container'>
+                <img src=` + projects[i].preview + ` class='img-fluid project-img'>
+            </div>
         </div>`;
 }
