5a8b378e1c6a9011dbb70790c2c7ab149c2e9ae1
Kaye Gamana <kayecgamana@gmail.com>
2025-06-05 05:05:28 +0800

    refined code - A07 preview and hover

--- ADET/assets/js/script.js
+++ ADET/assets/js/script.js
@@ -22,11 +22,6 @@ var projects = [
     {
         preview: "assets/images/projectPreviewComingSoon.png",
         hover: "assets/images/projectPreviewComingSoon.png",
-        alt: "Project4"
-    },
-    {
-        preview: "assets/images/projectPreviewComingSoon.png",
-        hover: "assets/images/projectPreviewComingSoon.png",
         alt: "Project5"
     },
     {
