e7930ca3fada6ce3d055e6f392ba2a500a7c981c
Kaye Gamana <kayecgamana@gmail.com>
2026-06-19 23:05:32 +0800

    Updated the config public and local

---
+++ configLocal.js
@@ -0,0 +1,11 @@
+/* ===========================================================
+   Environment Config [Local]
+   =========================================================== */
+
+window.PORTFOLIO_CONFIG = {
+  web3forms: {
+    main: "0fbe08ee-a40e-4e15-b332-9fa2cfd925bd",
+    hero: "0fbe08ee-a40e-4e15-b332-9fa2cfd925bd",
+    testimonial: "0fbe08ee-a40e-4e15-b332-9fa2cfd925bd"
+  }
+};
